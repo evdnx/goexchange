6f41533c3d1b05741efd58a15d2b9712a49b5312
ed <github.k50o8@passmail.net>
2025-11-18 15:49:32 +1100

    cleanup

--- exchange/logger.go
+++ exchange/logger.go
@@ -1,10 +1,29 @@
 package exchange
 
 import (
-	"github.com/evdnx/goexchange/internal/logutil"
+	"fmt"
+	"sync"
+
 	"github.com/evdnx/golog"
 )
 
+var (
+	sharedLogger     *golog.Logger
+	sharedLoggerOnce sync.Once
+	sharedLoggerErr  error
+)
+
 func defaultLogger() *golog.Logger {
-	return logutil.Default()
+	sharedLoggerOnce.Do(func() {
+		sharedLogger, sharedLoggerErr = golog.NewLogger(
+			golog.WithStdOutProvider(golog.ConsoleEncoder),
+			golog.WithLevel(golog.InfoLevel),
+		)
+	})
+
+	if sharedLoggerErr != nil {
+		panic(fmt.Sprintf("failed to initialize logger: %v", sharedLoggerErr))
+	}
+
+	return sharedLogger
 }
